bfbe0a09b97beeed684bb9f1a0b155d1d7cf8f9c
Mr. Brown <gitcherrypick.work@gmail.com>
2022-07-28 22:54:47 +0500

    System tests: Order, Cart.

--- test/system/carts_test.rb
+++ test/system/carts_test.rb
@@ -10,6 +10,26 @@ class CartsTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Carts"
   end
 
+  test "adding to Cart" do
+    visit store_index_url
+    click_on "Add to Cart", match: :first
+
+    assert_text "Your Cart"
+  end
+
+  test "emptying a Cart" do
+    visit store_index_url
+    click_on "Add to Cart", match: :first
+
+    assert_text "Your Cart"
+
+    page.accept_confirm do
+      click_on "Empty cart", match: :first
+    end
+
+    assert_no_text "Your Cart"
+  end
+
   test "destroying a Cart" do
     visit carts_url
     page.accept_confirm do
--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -49,4 +49,40 @@ class OrdersTest < ApplicationSystemTestCase
 
     assert_selector "#order_routing_number"
   end
+
+  test "credit card CC" do
+    visit store_index_url
+
+    click_on 'Add to Cart', match: :first
+
+    click_on 'Checkout'
+
+    fill_in 'order_name', with: 'Dave Thomas'
+    fill_in 'order_address', with: '123 Main Street'
+    fill_in 'order_email', with: 'dave@example.com'
+
+    assert_no_selector "#order_credit_card_number"
+
+    select 'Credit card', from: 'Pay type'
+
+    assert_selector "#order_credit_card_number"
+  end
+
+  test "purchase order" do
+    visit store_index_url
+
+    click_on 'Add to Cart', match: :first
+
+    click_on 'Checkout'
+
+    fill_in 'order_name', with: 'Dave Thomas'
+    fill_in 'order_address', with: '123 Main Street'
+    fill_in 'order_email', with: 'dave@example.com'
+
+    assert_no_selector "#order_po_number"
+
+    select 'Purchase order', from: 'Pay type'
+
+    assert_selector "#order_po_number"
+  end
 end
