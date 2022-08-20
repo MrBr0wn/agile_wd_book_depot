a10ec558e97f78c910d6371160a61170971d766b
Mr. Brown <gitcherrypick.work@gmail.com>
2022-08-20 19:54:11 +0500

    Prepare to merge.

--- config/routes.rb
+++ config/routes.rb
@@ -7,19 +7,28 @@ Rails.application.routes.draw do
   end
   get 'sessions/create'
   get 'sessions/destroy'
-  resources :users
-  resources :orders
-  root 'store#index', as: 'store_index'
   
-  resources :line_items do
-    collection do
-      get 'decrement(/:id)', to: 'line_items#decrement'
-    end
-  end
 
-  resources :carts
+  resources :users
   resources :products do
     get :who_bought, on: :member
   end
+
+  resources :support_requests, only: [:index, :update]
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
+  # resources :carts
+  # resources :orders
+  # root 'store#index', as: 'store_index'
+  # resources :line_items do
+  #   collection do
+  #     get 'decrement(/:id)', to: 'line_items#decrement'
+  #   end
+  # end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
